ecf88375bbe33829202a0521c2e452fe4944f2e2
code2828 <stevenhaocheng@163.com>
2023-06-06 12:28:23 +0800

    Initial Commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author zhanghaocheng
+ *
+ */
+module thulium {
+}
---
+++ src/tm/thulium/Thulium.java
@@ -0,0 +1,14 @@
+package tm.thulium;
+
+public class Thulium {
+
+	public Thulium() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello Thulium!");
+
+	}
+
+}
